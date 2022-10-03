b85749acc08c0efca6cf6a3f484d7873fece2c74
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-03 09:36:44 -0600

    cleaned

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,35 +1,34 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 export const putDb = async (content) => {
-  const jateDb = await openDB('jate',1);
-  const tx = jateDb.transaction('jate','readwrite');
-  const store = tx.objectStore('jate');
-  const request= store.put( {id:1, value:content});
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  
-  console.log(" PutDB",result);
-}
+
+  console.log(" PutDB", result);
+};
 
 export const getDb = async () => {
-  const jateDb = await openDB('jate',1);
-  const tx = jateDb.transaction('jate','readonly');
-  const store = tx.objectStore('jate');
-   const request = store.get({id:1});
-    const result = await request;
-    return result;
-
-}
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get({ id: 1 });
+  const result = await request;
+  return result;
+};
 
 initdb();
