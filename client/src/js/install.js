67bb292a68293455442717fab200d06e0cafa566
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-04 16:16:17 -0600

    butn should install now

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,16 +4,31 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    butInstall.style.visibility = 'visible';
+    window.deferredPrompt = event;
+    //butInstall.style.visibility = 'visible';
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     console.log('??');
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+     return;
+    }
+  
+    // Show prompt
+    promptEvent.prompt();
+        
+    // butInstall.style.visibility = 'hidden';
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
     butInstall.setAttribute('disabled', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
+    window.deferredPrompt = event;
 });
